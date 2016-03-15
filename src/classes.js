530ff73308527661aea5cb2c73bc15ede33a4064
Emily Znamierowski <emilyannz@gmail.com>
2016-03-15 16:59:01 -0500

    passed all tests

--- src/classes.js
+++ src/classes.js
@@ -1,5 +1,60 @@
 class Foo {
-
+  constructor(bar) {
+    this.bar = bar;
+  }
 }
 
-var bar;
+var bar = new Foo('bar')
+
+class Dog {
+  constructor(says) {
+    this.says = 'life is ruff';
+  }
+}
+
+class Cat {
+  growl() {
+    return 'meow';
+  }
+}
+
+var garfield = new Cat('garfield')
+
+class FacelessMan {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+var aMan = new FacelessMan(`Jaqen H'ghar`);
+
+
+
+class Lion extends Cat {
+  constructor() {
+    super();
+    this.size = 'BIG';
+  }
+}
+
+class KeepSecret {
+  constructor(secret) {
+    this.squeal = function() {
+      return secret;
+    }
+  }
+}
+
+class Key {
+  constructor() {}
+}
+
+class Safe {
+  constructor(secret, keyhole) {
+    this.unlock = function(key) {
+      if (key === keyhole) {
+        return secret;
+      }
+    }
+  }
+}
